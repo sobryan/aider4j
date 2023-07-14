bcab8085a9f8be5922300c2d81637d752aca775d
sobryan <33614669+sobryan@users.noreply.github.com>
2023-07-14 16:50:54 -0500

    initial

---
+++ src/main/java/com/github/sobryan/aider4j/Aider4jApplication.java
@@ -0,0 +1,13 @@
+package com.github.sobryan.aider4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aider4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aider4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/sobryan/aider4j/Aider4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sobryan.aider4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aider4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
